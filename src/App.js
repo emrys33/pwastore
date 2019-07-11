0bd7d28939792a4c6b8333c2f87fbd3c66b0906d
Parsa <parsa@pop-os.localdomain>
2019-07-12 04:19:16 +0430

    working-2

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './containers/Layout/Layout';
 import HomeApps from './containers/HomeApps/HomeApps';
 import AppDescription from './containers/AppDescription/AppDescription';
-import SignIn from './containers/MyProfile/Signin/Signin';
-import Signup from './containers/MyProfile/Signup/Signup';
+import SignIn from './containers/Auth/Signin/Signin';
+import Signup from './containers/Auth/Signup/Signup';
 import SearchResualt from './containers/SearchResualt/SearchResualt';
+import MyProfile from './containers/MyProfile/MyProfile';
 import About from './components/About/About';
 
 class App extends Component {
@@ -25,6 +26,7 @@ class App extends Component {
           <Route path='/about' component={About}/>
           <Route path='/signin' component={SignIn}/>
           <Route path='/signup' component={Signup}/>
+          <Route path='/myprofile' component={MyProfile}/>
           <Route path='/application' component={AppDescription}/>
           <Route path='/searchresualt' component={SearchResualt}/>
           <Route path='/' component={HomeApps}/>
---
+++ src/containers/MyProfile/MyProfile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MyProfile extends Component {
+    render () {
+        return(
+            <div></div>
+        );
+    };
+}
+
+export default MyProfile;
